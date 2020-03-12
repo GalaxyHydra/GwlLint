67c36ac1581a5a704df6f3137489e3a11d5493ba
Imran malik <imran.malik@galaxyweblinks.in>
2020-03-12 17:12:26 +0530

    Added framework with default rules for lint

---
+++ GwlLint.xcframework/ios-armv7_arm64/GwlLintProject.framework/Headers/GwlLintProject.h
@@ -0,0 +1,19 @@
+//
+//  GwlLintProject.h
+//  GwlLintProject
+//
+//  Created by gwl on 12/03/20.
+//  Copyright © 2020 gwl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GwlLintProject.
+FOUNDATION_EXPORT double GwlLintProjectVersionNumber;
+
+//! Project version string for GwlLintProject.
+FOUNDATION_EXPORT const unsigned char GwlLintProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GwlLintProject/PublicHeader.h>
+
+
